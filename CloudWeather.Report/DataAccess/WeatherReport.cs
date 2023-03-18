77db75605247785ab727bee000d6ce9f6c11e1ab
joeboxers28 <joeboxers28@yahoo.com>
2023-03-18 16:55:41 -0400

    Added data access for temperature and report microservices

---
+++ CloudWeather.Report/DataAccess/WeatherReport.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+
+namespace CloudWeather.Report.DataAccess
+{
+    public class WeatherReport
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public decimal TempHighF { get; set; }
+        public decimal TempLowF { get; set; }
+        public decimal RainfallTotalInches { get; set; }
+        public decimal SnowTotalInches { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
