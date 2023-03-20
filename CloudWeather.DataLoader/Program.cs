f9169efcc81518eab84bcf4981e3d4b8e69e1d0a
joeboxers28 <joeboxers28@yahoo.com>
2023-03-19 22:15:45 -0400

    Build initial data loader

--- CloudWeather.DataLoader/Program.cs
+++ CloudWeather.DataLoader/Program.cs
@@ -1,2 +1,138 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CloudWeather.DataLoader.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Json;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+var servicesConfig = config.GetSection("Services");
+
+var tempServiceConfig = servicesConfig.GetSection("Temperature");
+var tempServiceHost = tempServiceConfig["Host"];
+var tempServicePort = tempServiceConfig["Port"];
+
+var precipServiceConfig = servicesConfig.GetSection("Precipitation");
+var precipServiceHost = precipServiceConfig["Host"];
+var precipServicePort = precipServiceConfig["Port"];
+
+var zipCodes = new List<string>
+{
+    "73026",
+    "68104",
+    "04401",
+    "32808",
+    "19717"
+};
+
+Console.WriteLine("Starting data load");
+
+var temperatureHttpClient = new HttpClient();
+temperatureHttpClient.BaseAddress = new Uri($"http://{tempServiceHost}:{tempServicePort}");
+
+var precipHttpClient = new HttpClient();
+precipHttpClient.BaseAddress = new Uri($"http://{precipServiceHost}:{precipServicePort}");
+
+foreach (var zip in zipCodes)
+{
+    Console.WriteLine($"Processing Zip Code {zip}");
+    var from = DateTime.Now.AddYears(-2);
+    var thru = DateTime.Now;
+
+    for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+    {
+        var temps = PostTemp(zip, day, temperatureHttpClient);
+        PostPrecip(temps[0], zip, day, precipHttpClient);
+    }
+}
+
+void PostPrecip(int lowTemp, string zip, DateTime day, HttpClient httpClient)
+{
+    var rand = new Random();
+    var isPrecip = rand.Next(2) < 1;
+
+    PrecipitationModel precipitation;
+
+    if (isPrecip)
+    {
+        var precipInches = rand.Next(1, 16);
+        if (lowTemp < 32)
+        {
+            precipitation = new PrecipitationModel
+            {
+                AmountInches = precipInches,
+                WeatherType = "snow",
+                ZipCode = zip,
+                CreatedOn = day
+            };
+        }
+        else
+        {
+            precipitation = new PrecipitationModel
+            {
+                AmountInches = precipInches,
+                WeatherType = "rain",
+                ZipCode = zip,
+                CreatedOn = day
+            };
+        }
+    }
+    else
+    {
+        precipitation = new PrecipitationModel
+        {
+            AmountInches = 0,
+            WeatherType = "none",
+            ZipCode = zip,
+            CreatedOn = day
+        };
+    }
+
+    var precipResponse = httpClient
+        .PostAsJsonAsync("observation", precipitation)
+        .Result;
+
+    if (precipResponse.IsSuccessStatusCode)
+    {
+        Console.WriteLine("Posted Precipitation");
+        Console.WriteLine($"Zip: {zip}");
+        Console.WriteLine($"Type: {precipitation.WeatherType}");
+        Console.WriteLine($"Amount (in.): {precipitation.AmountInches}");
+    }
+}
+
+List<int> PostTemp(string zip, DateTime day, HttpClient httpClient)
+{
+    var rand = new Random();
+    var t1 = rand.Next(0, 100);
+    var t2 = rand.Next(0, 100);
+    var hiLoTemps = new List<int> { t1, t2 };
+    hiLoTemps.Sort();
+
+    var temperatureObservation = new TemperatureModel
+    {
+        TempLowF = hiLoTemps[0],
+        TempHighF = hiLoTemps[1],
+        ZipCode = zip,
+        CreatedOn = day
+    };
+
+    var temperatureResponse = httpClient
+        .PostAsJsonAsync("observation", temperatureObservation)
+        .Result;
+
+    if (temperatureResponse.IsSuccessStatusCode)
+    {
+        Console.WriteLine("Posted Temperature");
+        Console.WriteLine($"Zip: {zip}");
+        Console.WriteLine($"Low: {temperatureObservation.TempLowF}");
+        Console.WriteLine($"High: {temperatureObservation.TempHighF}");
+    }
+    else
+    {
+        Console.WriteLine(temperatureResponse.ToString());
+    }
+
+    return hiLoTemps;
+}
