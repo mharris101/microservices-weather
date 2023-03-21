67a99b1c365f7c9bb1b40b620ce3b7dd75f30cbf
joeboxers28 <joeboxers28@yahoo.com>
2023-03-20 22:04:09 -0400

    Fixed column names in weather report db

---
+++ CloudWeather.Report/Migrations/20230321015535_ChangeColumnNames.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudWeather.Report.Migrations
+{
+    public partial class ChangeColumnNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TempLowF",
+                table: "weather_report",
+                newName: "AverageLowF");
+
+            migrationBuilder.RenameColumn(
+                name: "TempHighF",
+                table: "weather_report",
+                newName: "AverageHighF");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AverageLowF",
+                table: "weather_report",
+                newName: "TempLowF");
+
+            migrationBuilder.RenameColumn(
+                name: "AverageHighF",
+                table: "weather_report",
+                newName: "TempHighF");
+        }
+    }
+}
