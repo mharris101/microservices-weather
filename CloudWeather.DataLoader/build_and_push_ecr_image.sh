74d27c5e41dc9d98a768466e6fa15643ccb97104
joeboxers28 <joeboxers28@yahoo.com>
2023-03-21 22:35:48 -0400

    fixed DataLoader script

--- CloudWeather.DataLoader/build_and_push_ecr_image.sh
+++ CloudWeather.DataLoader/build_and_push_ecr_image.sh
@@ -4,4 +4,4 @@ set -e
 aws ecr get-login-password --region us-east-1 --profile weather-ecr-agent | docker login --username AWS --password-stdin 185489668150.dkr.ecr.us-east-1.amazonaws.com
 docker build -f ./Dockerfile -t cloud-weather-data-loader:latest .
 docker tag cloud-weather-data-loader:latest 185489668150.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-data-loader:latest
-docker push 185489668150.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-tempdata-loadererature:latest
+docker push 185489668150.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-data-loader:latest
